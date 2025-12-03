fa2004b34698fda3fd7e9ced55e942ece88cd93a
Vinicius Marconatto <110271316+VMarconatto@users.noreply.github.com>
2025-12-03 13:08:53 -0300

    Update auth.routes.tsx

--- frontend/src/routes/auth.routes.tsx
+++ frontend/src/routes/auth.routes.tsx
@@ -1,11 +1,4 @@
-/**
-** =======================================================
-@SECTION : React Router — Auth Routes (Públicas)
-@FILE : auth.routes.tsx
-@PURPOSE : Declarar as rotas públicas de autenticação (ex.: Signin) usando React Router v6.
-@LAST_EDIT : 2025-11-10
-** =======================================================
-*/
+
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
